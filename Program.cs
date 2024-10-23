ef285698e9cb42debd91319afade2557153d8a10
sedef <sedef@SEDEF>
2024-10-23 20:37:42 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim231004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("birinci sayıyı giriniz");
+            int sayi1= Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("ikinci sayıyı giriniz");
+            int sayi2= Convert.ToInt16(Console.ReadLine());
+            int toplam = sayi1 + sayi2;
+            int fark = sayi1 - sayi2;
+            int carpim = sayi1 * sayi2;
+            int bolum = sayi1 / sayi2;
+            Console.WriteLine($"iki sayının toplamı {toplam}");
+            Console.WriteLine($"iki sayının farkı {fark}");
+            Console.WriteLine($"iki sayının çarpımı {carpim}");
+            Console.WriteLine($"iki sayının bölümü {bolum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("yazilim231004")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("yazilim231004")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c914cf3d-e5d6-472b-a568-090f051f8dea")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
